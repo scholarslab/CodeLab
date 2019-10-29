f53c60a016f9baf86c738aa061b59c7ef2d8708a
Shane Lin <ssl2ab@virginia.edu>
2019-10-29 15:53:00 -0400

    add answer for assignment 0x01

--- Week09/answers/0x00.py
+++ Week09/answers/0x00.py
@@ -10,8 +10,9 @@ for line in text.split("\n\n"):
     if not name.isupper() or not name.endswith("."):
         continue
     # Up to this point, it's more or less the same as the Week05 assignment
-    line_dict = {}
-    line_dict["role"] = name[:-1] #[:-1] omits the ending period
+
+    line_dict = {} # create a new dictionary to keep track of the dialog line 
+    line_dict["role"] = name[:-1] #[:-1] omits the ending period in the name
     line_dict["dialog"] = " ".join(line.split("\n")[1:]) #we split the dialog line on new line characters ("\n")
     # take all the text lines after the first line (which we know now is the role name)
     # then join them on spaces (we could join on new lines if we wanted to retain them)
