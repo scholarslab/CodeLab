91ef4c2de28c72a3dd5121245619d9c343f5b7bc
Shane Lin <ssl2ab@virginia.edu>
2019-12-04 14:32:27 -0500

    week 13 hw?!

---
+++ Week13/README.md
@@ -0,0 +1,17 @@
+# Weeks 13+14: HTML/CSS/Design
+
+![assets/hazel_sniff.jpg](assets/hazel_sniff.jpg)
+
+Even though I've lost custody of all of you this week and next, I'm still going to assign homework.
+
+## Assignment
+### Part Alpha
+
+Write a script to download the list of Scholars' Lab blog posts (https://scholarslab.lib.virginia.edu/blog/) and then figure out how many blog posts each other has.
+
+The page highlights the most recent four posts. These are optional because they don't conform to the standard structure of the majority of older posts below.
+
+You can assume there aren't people with identical names. You don't have to sort the results, but it might be useful to figure out how such a thing could be done. 
+
+### Part Bravo
+Have a good break and keep on hacking! 
--- Week13/answers/part_alpha.py
+++ Week13/answers/part_alpha.py
@@ -1,16 +1,22 @@
 from bs4 import BeautifulSoup
 import requests
 
+
 url = "https://scholarslab.lib.virginia.edu/blog/"
 html = requests.get(url).text
-soup = BeautifulSoup(html)
+soup = BeautifulSoup(html, features="html.parser")
 
+# We're going to use a dictionary to store our counter.
+# Dictionaries map keys to values, like how a phone book maps names to numbers.
+# The key is unique, so using a dictionary as a counter makes it easy
+# to keep track of people by using their names as keys.
+# Their post count will be the value.
 counter = {}
-
+total = 0
 # We can use the nth-child CSS selector to select the
 # *second* link for each blog post or we can just get
 # the <li> tag for each post and use BS4's DOM traversal
-# mecahnisms like find_all() to get to that second <a>
+# mechanisms like find_all() to get to that second <a>
 # tag
 for link in soup.select("ul.page-previous_posts li a:nth-child(2)"):
     # Because of how we constructed our soup.select, the link
@@ -18,6 +24,7 @@ for link in soup.select("ul.page-previous_posts li a:nth-child(2)"):
     # blog post author's profile page and link.get_text() is
     # the author's name.
     author = link.get_text()
+    total += 1
     if author in counter:
         # if the author is already in the list of authors,
         # increment the counter 
@@ -27,5 +34,13 @@ for link in soup.select("ul.page-previous_posts li a:nth-child(2)"):
         # the blog post we're looking at
         counter[author] = 1
 
-sorted_counter = {author:count for author, count in sorted(counter.items(), key=lambda x: x[1])}
-print(sorted_counter)
+# Here's a bit of extra code to sort the dictionary by number of posts
+# You don't have to do this or spend too much time understanding how this works
+sorted_counter = {author:count for author, count in sorted(counter.items(), key=lambda x: x[1], reverse=True)}
+
+# Counter and sorted_counter are dictionaries that map the name to number of posts
+# We can print each author and their posts using a for loop.
+# In a for loop, the iteration cycles through keys (authors, in this case).
+print("Total Posts:",total,"\n")
+for author in sorted_counter:
+    print(author, ":", sorted_counter[author])
