b80b44ccbd5856ae0fde2485dd270cabb51b18f1
walshbr <bmw9t@virginia.edu>
2021-09-29 13:25:19 -0400

    Sharing the second answer

--- debugging/2.py
+++ debugging/2.py
@@ -2,9 +2,9 @@
 #  out a pig latin translation.
 
 # Request word & store as variable "bacon"
-piglatin=inputs("Which word do you want to translate?\n")
+piglatin=input("Which word do you want to translate?\n")
 
 # For "pig", remove character at position[0], suffix with that character,
 # suffix with "ay"
 
-print[pigslatin[1:]+piglatin[0]"ay"
+print(piglatin[1:]+piglatin[0] + "ay")
