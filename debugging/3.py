eb64d45de8dd19d8589376e7b8522fff9601da75
walshbr <bmw9t@virginia.edu>
2021-09-29 13:55:48 -0400

    Update the third exercise

--- debugging/3.py
+++ debugging/3.py
@@ -1,10 +1,11 @@
 # Create a function that will filter evens from a list of numbers
 
-define filter_evens list: 
-    even_numbers=()
-    for number in list: 
+def filter_evens(list):
+    # take a list of numbers and return only the even ones 
+    even_numbers=[]
+    for num in list: 
         if num / 2 == 0: 
-            even_numbers.add(num)
+            even_numbers.append(num)
     return even_numbers
 nums = [0,-2,3,14]
-print(filter_evens nums )
+print(filter_evens(nums))
