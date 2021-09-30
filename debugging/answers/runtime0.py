979a44741db04e6d8702fcbdb782b1c44de0384c
shane-et-al <shane.ext@gmail.com>
2021-09-30 14:03:17 -0400

    add answers for runtime debug exercises

---
+++ debugging/answers/runtime0.py
@@ -0,0 +1,12 @@
+# Returns True if the list is sorted
+# Returns False if the list is not sorted
+def is_sorted(numbers):
+    i = 0
+    while i < len(numbers)-1:
+        if numbers[i] > numbers[i+1]:
+            return(False)
+        i+=1
+    return(True)
+
+print("This should be True: ", is_sorted([1,4,6,7,8,9]))
+print("This should be False: ", is_sorted([3,6,2,5,7]))
---
+++ debugging/answers/runtime1.py
@@ -0,0 +1,26 @@
+# Find the highest point
+# Input: a matrix of height values,
+#        representing a topographic map
+# Output: the highest point in the map
+def find_peak(map):
+    # Starting coordinates
+    x = 0
+    y = 0
+    # floor height
+    peak = 0
+    while y < len(map):
+        x = 0
+        while x < len(map[y]):
+            if map[x][y] > peak:
+                peak = map[x][y]
+            x+=1
+        y+=1
+    return(peak)
+
+# A topographic map
+map = [[5,3,1,3,4],
+       [1,3,3,5,4],
+       [3,2,4,5,6],
+       [6,4,1,7,4],
+       [3,4,4,5,3]]
+print(find_peak(map))
