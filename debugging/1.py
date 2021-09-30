5ec60a33471b5297b6aec8da501d1020a19fa647
shane-et-al <ssl2ab@virginia.edu>
2021-09-30 10:36:42 -0400

    add corrected code banner

--- debugging/1.py
+++ debugging/1.py
@@ -1,7 +1,7 @@
+### THIS EXERCISE CODE HAS BEEN CORRECTED ALREADY ###
+
 # For these exercises, you should focus on debugging - looking for errors. You should not write new code.
-
 # for each exercise, we'll comment what the code is supposed to do and then debug it together
-
 # Stores "hello world" to the screen and prints it to the screen
 
 
